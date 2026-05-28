71c7db7111457d6c3392d94264c962c9293d7562
Teddy Andriamam <te2d@Mac.lan>
2026-05-28 16:23:21 +0200

    feat: GET /stations/{id} returns 200 or 404

--- server/main.go
+++ server/main.go
@@ -24,5 +24,6 @@ func main() {
 		fmt.Fprintln(w, "ok")
 	})
 	mux.HandleFunc("GET /stations", app.listStations)
+	mux.HandleFunc("GET /stations/{id}", app.getStation)
 	http.ListenAndServe(":8080", mux)
 }
